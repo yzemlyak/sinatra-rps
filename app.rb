adb3a25d9aa3b5ef3ee22398786ae08e3fdc54e7
yzemlyak <yzemlyak@gmail.com>
2025-04-14 23:51:32 +0000

    Added three options + top page links

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ get("/") do
   "
 end
 
-get("/rock")do 
+get("/rock") do 
   moves = ["rock", "paper", "scissors"]  
   
   @comp_move = moves.sample
@@ -21,5 +21,37 @@ get("/rock")do
     @outcome = "won"
   end
   
-erb(:zebra)
+  erb(:rocky)
+end
+
+get("/paper") do 
+  moves = ["rock", "paper", "scissors"]  
+  
+  @comp_move = moves.sample
+  
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:papery)
+end
+
+get("/scissors") do 
+  moves = ["rock", "paper", "scissors"]  
+  
+  @comp_move = moves.sample
+  
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:scissory)
 end
