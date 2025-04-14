b2058e3f7b7bf3479cc2ac8438a42b33e359da5e
yzemlyak <yzemlyak@gmail.com>
2025-04-14 23:56:05 +0000

    added home page

--- app.rb
+++ app.rb
@@ -2,10 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homey)
 end
 
 get("/rock") do 
