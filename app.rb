0848a1620af26fb90dd0fff8ac4b87531128c2e9
yzemlyak <yzemlyak@gmail.com>
2025-04-14 23:21:46 +0000

    First build-out for rock route

--- app.rb
+++ app.rb
@@ -7,3 +7,28 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock")do 
+  moves = ["rock", "paper", "scissors"]  
+  comp_move = moves.sample
+  if comp_move == "rock"
+    outcome = "tied"
+  elsif comp_move == "paper"
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
+"
+    <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+   They played #{comp_move}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
